39ac216751c899f651aaf06b49b09e18fc8131cc
Wanjirundegwa085 <wanjirundegwa085@gmail.com>
2022-03-28 13:58:31 -0700

    My second try

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -20,3 +20,28 @@ unsigned int _strspn(char *s, char *accept)
 		if (in_accept(s[i], accept))
 			length++;
 		else
+			break;
+	}
+
+	return (length);
+}
+
+/**
+ * in_accept - checks if agiven character is in the string accept
+ * @c: character to be checked
+ * @accept: string containingthe only accepted characters
+ *
+ * Return: 1 if c is in accept. Otherwise 0
+ */
+int in_accept(char c, char *accept)
+{
+	int i;
+
+	for (i = 0; accept[i] != '\0'; i++)
+	{
+		if (c == accept[i])
+			return (1);
+	}
+
+	return (0);
+}
